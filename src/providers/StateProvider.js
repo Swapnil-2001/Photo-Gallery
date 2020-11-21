d15785c36f244f634913a05be532aa2b16f97410
Swapnil Sengupta <53232360+Swapnil-2001@users.noreply.github.com>
2020-11-21 14:31:46 +0530

    Update StateProvider.js

--- src/providers/StateProvider.js
+++ src/providers/StateProvider.js
@@ -1,6 +1,6 @@
 import React, { createContext, useContext, useReducer } from "react";
 
-// Create data layer to track the basket
+// Create data layer
 export const StateContext = createContext();
 
 // Build Provider, give it access to data layer
